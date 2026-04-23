59cb902f6821136d0d573311366b39aceb184f8a
devadathansp <108550599+dev-1729@users.noreply.github.com>
2026-04-23 23:05:37 +0530

    Switch password hashing from bcrypt to pbkdf2_sha256

--- auth.py
+++ auth.py
@@ -1,20 +1,24 @@
 import sqlite3
-from passlib.hash import bcrypt
+from passlib.hash import pbkdf2_sha256
 
 def create_user(username, password):
     conn = sqlite3.connect("chat.db")
     c = conn.cursor()
 
-    hashed = bcrypt.hash(password)
+    hashed = pbkdf2_sha256.hash(password)
 
     try:
-        c.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashed))
+        c.execute(
+            "INSERT INTO users (username, password) VALUES (?, ?)",
+            (username, hashed)
+        )
         conn.commit()
         return True
     except:
         return False
     finally:
         conn.close()
+
 
 def login_user(username, password):
     conn = sqlite3.connect("chat.db")
@@ -25,6 +29,7 @@ def login_user(username, password):
 
     conn.close()
 
-    if user and bcrypt.verify(password, user[1]):
-        return user[0]  # user_id
+    if user and pbkdf2_sha256.verify(password, user[1]):
+        return user[0]
+
     return None
